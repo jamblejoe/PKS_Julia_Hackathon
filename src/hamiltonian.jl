f3849e3b47c3244f55a14ac1c1ed079b71359bbb
Paul <schindlerpaul@arcor.de>
2022-05-20 21:54:59 +0200

    long range update

--- src/hamiltonian.jl
+++ src/hamiltonian.jl
@@ -1,15 +1,25 @@
 using Random, LinearAlgebra
 
-function simple_hamiltonian(L, hopping, width, onsite)
+function nn_hamiltonian(L, hopping, onsite)
     hamiltonian_arr = zeros(L,L)
 
     for i in 1:L
         # diagonal
-        hamiltonian_arr[i,i] = randn()*width + onsite
+        hamiltonian_arr[i,i] = onsite
 
         # off-diagonal
         hamiltonian_arr[i, (i+1)%L+1] = hopping
         hamiltonian_arr[(i+1)%L+1, i] = hopping
+    end
+    return hamiltonian_arr
+end
+
+function simple_hamiltonian(L, hopping, width, onsite)
+    hamiltonian_arr = nn_hamiltonian(L, hopping, onsite)
+
+    for i in 1:L
+        # diagonal
+        hamiltonian_arr[i,i] = randn()*width 
     end
     return hamiltonian_arr
 end
@@ -29,3 +39,14 @@ function long_range(L::Int, power::Float64, on_site::Float64)
     
     return hamiltonian_arr
 end;
+
+function add_onsite_disorder(hamiltonian_array::Array, disorder)
+    L = size(hamiltonian_array)[0]
+    disordered_arr = hamiltonian_array
+    for i in 1:L
+        disordered_arr[i, i] = disordered_arr[i,i] + disorder * randn()
+    end
+    return disordered_arr
+end
+
+
