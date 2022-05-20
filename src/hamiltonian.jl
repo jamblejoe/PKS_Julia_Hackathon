d36c51a123a86199e30bf807486569b5a7daea2a
Paul <schindlerpaul@arcor.de>
2022-05-20 21:51:18 +0200

    long range hamiltonian

--- src/hamiltonian.jl
+++ src/hamiltonian.jl
@@ -14,3 +14,18 @@ function simple_hamiltonian(L, hopping, width, onsite)
     return hamiltonian_arr
 end
 
+function long_range(L::Int, power::Float64, on_site::Float64)
+    hamiltonian_arr = Array{Float64}(undef, L, L) 
+    
+    for i in 1:L
+        
+        # long-range coupling
+        for j in 1:L
+            dist                 = min(abs(i-j), abs(L-i+j)) #pbc
+            hamiltonian_arr[i,j] = dist^(-power)
+        hamiltonian_arr[i,i] = on_site
+        end
+    end
+    
+    return hamiltonian_arr
+end;
