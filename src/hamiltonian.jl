849a1c925160f50929e45f91c7ddbc6926a282cf
placke <placke@pks.mpg.de>
2022-05-20 22:20:42 +0200

    hi

--- src/hamiltonian.jl
+++ src/hamiltonian.jl
@@ -1,11 +1,11 @@
 using Random, LinearAlgebra
 
 function simple_hamiltonian(L, hopping, width, onsite)
-    hamiltonian_arr = zeros(L,L)
+    hamiltonian_arr = zeros(L, L)
 
     for i in 1:L
         # diagonal
-        hamiltonian_arr[i,i] = randn()*width + onsite
+        hamiltonian_arr[i, i] = randn() * width + onsite
 
         # off-diagonal
         hamiltonian_arr[i, (i+1)%L+1] = hopping
