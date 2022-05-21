1715379d967bf1a577ca8ec2ec699e37e1fc8f8e
suzycheung <szhang@pks.mpg.de>
2022-05-21 14:33:21 +0200

    edited  the function

--- src/agr.jl
+++ src/agr.jl
@@ -1,10 +1,10 @@
 using Statistics
 # This is averaged over the spectrum but not yet disorder
 function agr(eigvals::Vector{Float64})
-    ratios = Vector{Float64}(undef, length(eigvals)-1)
-    for i in 1:length(eigvals)-1
-        level1 = eigvals[i]
-        level2 = eigvals[i+1]
+    ratios = Vector{Float64}(undef, length(eigvals)-2)
+    for i in 1:length(eigvals)-2
+        level1 = eigvals[i+2] - eigvals[i]
+        level2 = eigvals[i+1] - eigvals[i]
         if level1 < level2
             ratios[i] = level1/level2
         else
